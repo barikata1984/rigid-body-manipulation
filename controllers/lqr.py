a2f0dd025fd9d035a1520d945c3e26e0e7a3b416
barikata1984 <franche1984@gmail.com>
2025-05-29 23:49:46 -0400

    now core.py and lqr.py has been formatted.

--- controllers/lqr.py
+++ controllers/lqr.py
@@ -2,50 +2,52 @@ from dataclasses import dataclass, field
 
 import numpy as np
 from mujoco._structs import MjData, MjModel
-from numpy.typing import ArrayLike, NDArray
+from numpy.typing import NDArray
 from omegaconf import MISSING
 from omegaconf.errors import MissingMandatoryValue
 from scipy import linalg
 
-from dynamics import StateSpaceConfig, StateSpace
+from dynamics import StateSpace, StateSpaceConfig
 
 
 @dataclass
 class LinearQuadraticRegulatorConfig:
     target_class: str = "LinearQuadraticRegulator"
-    state_space: StateSpaceConfig = StateSpaceConfig()
+    state_space: StateSpaceConfig = field(default_factory=StateSpaceConfig)
     input_gain: list[float] = MISSING
 
 
 class LinearQuadraticRegulator:
-    def __init__(self,
-                 cfg: LinearQuadraticRegulatorConfig,
-                 m: MjModel,
-                 d: MjData,
-                 ) -> None:
-
+    def __init__(
+        self,
+        cfg: LinearQuadraticRegulatorConfig,
+        m: MjModel,
+        d: MjData,
+    ) -> None:
         # Fill a potentially missing field of a planner configuration
         try:
             cfg.input_gain
         except MissingMandatoryValue:
             cfg.input_gain = np.ones(m.nu).tolist()  # awkward but omegaconf
-                                                      # does not support NDArray
+            # does not support NDArray
 
         self.ss = StateSpace(cfg.state_space, m, d)
         self.input_gain = cfg.input_gain
         self.gain_matrix = self.update_control_gain(m, d)
 
-    def update_control_gain(self,
-                            m: MjModel,
-                            d: MjData,
-                            ) -> NDArray:
+    def update_control_gain(
+        self,
+        m: MjModel,
+        d: MjData,
+    ) -> NDArray:
         self.ss.update_matrices(m, d)
 
-        Q = np.eye(self.ss.ns)  # Initial state cost matrix R = np.diag(self.input_gains)  # Input gain matrix
+        Q = np.eye(
+            self.ss.ns
+        )  # Initial state cost matrix R = np.diag(self.input_gains)  # Input gain matrix
         R = np.diag(self.input_gain)  # Input gain matrix
         # Compute the feedback gain matrix K
         P = linalg.solve_discrete_are(self.ss.A, self.ss.B, Q, R)
         K = linalg.pinv(R + self.ss.B.T @ P @ self.ss.B) @ self.ss.B.T @ P @ self.ss.A
 
         return K
-
