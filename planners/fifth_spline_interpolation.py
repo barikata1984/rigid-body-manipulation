f50b05fe03127e57ea8cac5b948e227bc9bb25b2
barikata1984 <franche1984@gmail.com>
2025-07-24 14:55:09 -0400

    added comments to the 5th order spline interpolation of trajectory

--- planners/fifth_spline_interpolation.py
+++ planners/fifth_spline_interpolation.py
@@ -16,8 +16,27 @@ def get_trajectory_interpolated_with_fifth_spline(
     t_s = init_step
     t_e = t_s + n_steps
 
-    # Define normalized boundaries of a spline
-    normalized_bounds = np.array(
+    # define the parameter matrix for a differentiated fifth-order polynomial
+    boundary_matrix = np.array(
+        [
+            # start pos
+            [t_s**5, t_s**4, t_s**3, t_s**2, t_s, 1],
+            # end pos
+            [t_e**5, t_e**4, t_e**3, t_e**2, t_e, 1],
+            # start vel
+            [5 * t_s**4, 4 * t_s**3, 3 * t_s**2, 2 * t_s**1, 1, 0],
+            # end vel
+            [5 * t_e**4, 4 * t_e**3, 3 * t_e**2, 2 * t_e**1, 1, 0],
+            # start acc
+            [20 * t_s**3, 12 * t_s**2, 6 * t_s**1, 2, 0, 0],
+            # end vel
+            [20 * t_e**3, 12 * t_e**2, 6 * t_e**1, 2, 0, 0],
+        ],
+        dtype=float,
+    )
+
+    # Define normalized boundaries of a spline.
+    normalized_boundaries = np.array(
         [
             0,  # start pos
             1,  # end pos
@@ -28,33 +47,26 @@ def get_trajectory_interpolated_with_fifth_spline(
         ]
     )
 
-    # define the parameter matrix for a differentiated fifth-order polynomial
-    spline_matrix = np.array(
-        [
-            [t_s**5, t_s**4, t_s**3, t_s**2, t_s, 1],
-            [t_e**5, t_e**4, t_e**3, t_e**2, t_e, 1],
-            [5 * t_s**4, 4 * t_s**3, 3 * t_s**2, 2 * t_s**1, 1, 0],
-            [5 * t_e**4, 4 * t_e**3, 3 * t_e**2, 2 * t_e**1, 1, 0],
-            [20 * t_s**3, 12 * t_s**2, 6 * t_s**1, 2, 0, 0],
-            [20 * t_e**3, 12 * t_e**2, 6 * t_e**1, 2, 0, 0],
-        ],
-        dtype=float,
+    # Find coefficient of the 5th-order spline s.t.
+    # boundary_matreix @ coeffs = normalized_boundaries  # shape: (6, 6) @ (6,) = (6,)
+    coeffs = la.solve(
+        boundary_matrix,
+        normalized_boundaries,  # boundary values
     )
-
-    # compute the constants of the fifth-order spline
-    coeffs = la.solve(spline_matrix, normalized_bounds).squeeze()
 
     displacement = np.array(displacement)
     pos_offset = np.array(pos_offset)
 
     def plan(step: int):
-        fifth = np.array([step**i for i in range(5, -1, -1)])
-        fourth = np.array([step**i * (i + 1) for i in range(4, -1, -1)])
-        third = np.array([step**i * (i + 1) * (i + 2) for i in range(3, -1, -1)])
+        fifth = np.array([step**i for i in range(5, -1, -1)])  # 5th to 0th (6 elems in total))
+        fourth = np.array([step**i * (i + 1) for i in range(4, -1, -1)])  # 4th to 0th (5 elems in total)
+        third = np.array([step**i * (i + 1) * (i + 2) for i in range(3, -1, -1)])  # 3rd to 0th  (4 elems in total))
 
-        pos = displacement * np.dot(coeffs[:], fifth) + pos_offset
-        vel = displacement * np.dot(coeffs[:-1], fourth) / timestep
-        acc = displacement * np.dot(coeffs[:-2], third) / timestep**2
+        # Since the spline only outputs normalized trajectory, the This boundary vector is multiplied with the displacement (dot product) later
+        # to get the actual pos/vel/acc.
+        pos = displacement * np.dot(coeffs[:], fifth) + pos_offset  # unit: [m] ← [m] * [.]  + [m]
+        vel = displacement * np.dot(coeffs[:-1], fourth) / timestep  # unit: [m/s] ← ([m] * [.]  + [m]) / [s]
+        acc = displacement * np.dot(coeffs[:-2], third) / timestep**2  # unit: [m/s^2] ← ([m] * [.]  + [m]) / [s^2]
 
         return np.array([pos, vel, acc])
 
